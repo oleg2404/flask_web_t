3de297253ca6d3834deaf460614a375966f66ec3
oleg2404 <35565527+oleg2404@users.noreply.github.com>
2021-10-20 15:37:52 +0300

    Finished

--- website/models.py
+++ website/models.py
@@ -6,12 +6,12 @@ from sqlalchemy.sql import func
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezonw = True), default = func.now())
-    user_id = db.Column(db.Integer, db.foreign_key("user.id")) #Refrance User.id (must be lowCase user)
+    date = db.Column(db.DateTime(timezone = True), default = func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(150), unique = True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship("Note")#Refrance Note Class(Must be UpperCase)
+    notes = db.relationship("Note")
